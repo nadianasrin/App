58161fb7406d1f781e15a2a1c022ad8662f3c0d1
zubayerhimel <zhimel007@gmail.com>
2019-11-09 00:32:27 +0600

    successfully routed to signup signin link

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,38 +1,18 @@
-using System.Threading.Tasks;
-using App.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.Controllers
 {
     public class AccountController : Controller
     {
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
-
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ApplicationDbContext context)
+        // Request to view account/signup
+        public IActionResult Signup()
         {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _context = context;
-        }
-
-        public IActionResult Register(string returnUrl)
-        {
-            ViewBag.returnurl = returnUrl;
             return View();
         }
 
-        public IActionResult Login(string returnUrl)
+        public IActionResult Signin()
         {
-            ViewBag.returnurl = returnUrl;
-            return View();
-        }
-
-        public async Task<IActionResult> Register(Registration registration, string returnUrl)
-        {
-            return View(registration);
+            return View();  
         }
 
     }
