e06e0dc0d7ca69961b4afdd2adca8979c65ceffc
zubayerhimel <zhimel007@gmail.com>
2019-11-09 13:14:11 +0600

    successfully registered

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,23 @@
+using System.Threading.Tasks;
+using App.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+
         // Request to view account/signup
         public IActionResult Signup()
         {
@@ -15,5 +29,26 @@ namespace App.Controllers
             return View();  
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Signup(Registration registration)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new ApplicationUser{UserName = registration.StudentVarsityId, Email = registration.RegUserEmail};
+                var result = await _userManager.CreateAsync(user, registration.RegUserPassword);
+
+                if(result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("signin", "account"); 
+                }
+
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(registration);
+        }
     }
 }
